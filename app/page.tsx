18ac207ff7bc9b5deb8671e34c4b667ccf32d11f
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-06-03 02:53:58 +0300

    added recent projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome /> },
-            { name: "About", link: "#about" },
-            { name: "Projects", link: "#projects" },
-            { name: "Contact", link: "#contact" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
