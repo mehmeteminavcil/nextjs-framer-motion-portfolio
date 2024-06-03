6bde91a17218e1d52de56826ba3f399b5226a1d5
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-06-03 23:43:01 +0300

    added sentry userfeedback etc & testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Client from "@/components/Client";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Client />
       </div>
     </main>
   );
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0e7d17b11ff7cef71f6a26fcbe7b6840@o4507368968421376.ingest.de.sentry.io/4507368978972752",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
