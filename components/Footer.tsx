48722f7eb4bf8ca4332bd039c616eafaf3a31efa
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-06-04 17:21:00 +0300

    added Approach & Experience & Footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg
               saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img src={profile.img} alt="social icon" width={20} height={20} />
             </div>
           ))}
         </div>
